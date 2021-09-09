a272b70cbe5ddb92d992521e7acbb6a646836b82
Jérôme Mulsant <jerome@rue-de-la-vieille.fr>
2021-09-09 17:43:53 +0200

    Config display improvements

--- email-smtp.php
+++ email-smtp.php
@@ -32,15 +32,31 @@ class EmailSmtp
 {
     const TEXTDOMAIN = 'email-smtp';
 
-    const SLUG = 'smtp';
+    private const PARENT_SLUG = 'tools.php';
+    private const PAGE_SLUG = 'smtp';
+
+    const PROP_FROM = 'From';
+    const PROP_FROM_NAME = 'FromName';
+    const PROP_SENDER = 'Sender';
+    const PROP_MAILER = 'Mailer';
+    const PROP_HOST = 'Host';
+    const PROP_SMTP_AUTH = 'SMTPAuth';
+    const PROP_USER = 'Username';
+    const PROP_PASS = 'Password';
+    const PROP_SMTP_SECURE = 'SMTPSecure';
+    const PROP_PORT = 'Port';
 
     const PHPMAILER_PROPERTIES = [
-        'Host'       => 'EMAIL_SMTP_HOST',
-        'SMTPAuth'   => 'EMAIL_SMTP_AUTH',
-        'Username'   => 'EMAIL_SMTP_USERNAME',
-        'Password'   => 'EMAIL_SMTP_PASSWORD',
-        'SMTPSecure' => 'EMAIL_SMTP_SECURE',
-        'Port'       => 'EMAIL_SMTP_PORT',
+        self::PROP_FROM_NAME   => 'EMAIL_FROM_NAME',
+        self::PROP_FROM        => 'EMAIL_FROM_EMAIL',
+        self::PROP_SENDER      => 'EMAIL_FROM_EMAIL',
+        self::PROP_HOST        => 'EMAIL_SMTP_HOST',
+        self::PROP_MAILER      => null,
+        self::PROP_SMTP_AUTH   => 'EMAIL_SMTP_AUTH',
+        self::PROP_USER        => 'EMAIL_SMTP_USERNAME',
+        self::PROP_PASS        => 'EMAIL_SMTP_PASSWORD',
+        self::PROP_SMTP_SECURE => 'EMAIL_SMTP_SECURE',
+        self::PROP_PORT        => 'EMAIL_SMTP_PORT',
     ];
 
     public function __construct()
@@ -81,11 +97,11 @@ class EmailSmtp
     public function admin_menu()
     {
         add_submenu_page(
-            'tools.php',
+            self::PARENT_SLUG,
             __('Email &amp; SMTP', 'email-smtp'),
             __('Email &amp; SMTP', 'email-smtp'),
             'manage_options',
-            self::SLUG,
+            self::PAGE_SLUG,
             [$this, 'info_page']
         );
     }
@@ -93,8 +109,6 @@ class EmailSmtp
     public function info_page()
     {
         global $title;
-
-        $phpmailer = $this->get_phpmailer();
 
         echo '<div class="wrap">';
         echo '<h1>' . $title . '</h1>';
@@ -105,77 +119,36 @@ class EmailSmtp
             echo $test_output;
         }
 
-        $config = $this->get_mailer_configuration();
-
-        if (defined('EMAIL_SMTP_HOST') && EMAIL_SMTP_HOST !== null) {
-            if (!$this->check_phpmailer_configuration()) {
-                echo '<div class="notice notice-error">';
-                echo '<p>';
-                esc_html_e('A plugin is overriding the configuration with these differences:', 'email-smtp');
-                echo '</p>';
-
-                $diff = [];
-                foreach ($config as $key => $value) {
-                    if (!isset($phpmailer->{$key}) || $phpmailer->{$key} !== $value) {
-                        $diff[$key] = $phpmailer->{$key};
-                    }
-                }
-                $this->display_config($diff);
-
-                echo '</div>';
-            }
-
-            echo '<p>';
-            echo sprintf(
-            /* translators: Placeholder is a PHP file */
-                __('Parameters loaded from %s:', 'email-smtp'),
-                '<code>wp-config.php</code>'
-            );
-            echo '</p>';
-
-            $this->display_config($config);
-        } else {
-            echo '<p>';
-            echo sprintf(
-            /* translators: Placeholder is a PHP file */
-                __('No email configuration in %s.', 'email-smtp'),
-                '<code>wp-config.php</code>'
-            );
-            echo '</p>';
-            if ($phpmailer->Mailer !== 'smtp') {
-                echo '<p>';
-                echo __('Current configuration:', 'email-smtp') . ' ';
-                echo sprintf(
-                /* translators: Placeholder is a PHP function */
-                    __('PHP %s function.', 'email-smtp'),
-                    '<code>mail()</code>'
-                );
-                echo '</p>';
-            } else {
-                echo '<p>';
-                echo __('Current configuration:', 'email-smtp');
-                echo '</p>';
-                $this->display_config(array_filter((array)$phpmailer));
-            }
-        }
-
         if (defined('EMAIL_FORCE_TO') && !empty(EMAIL_FORCE_TO)) {
-            echo '<hr>';
-            echo '<p>';
             printf(
-            // translators: %1$s is the defined constant, %2$s is the email address.
-                esc_html__(
-                    '%1$s is defined: all outgoing messages except test messages will be send to %2$s with no other recipients, nor Cc or Bcc.',
-                    'email-smtp'
-                ),
-                '<code>EMAIL_FORCE_TO</code>',
-                '<code>' . EMAIL_FORCE_TO . '</code>'
+                '<div class="notice notice-info"><p>%s</p></div>',
+                sprintf(
+                // translators: %1$s is the defined constant, %2$s is the email address.
+                    esc_html__(
+                        '%1$s is defined: all outgoing messages except test messages will be send to %2$s with no other recipients, nor Cc or Bcc.',
+                        'email-smtp'
+                    ),
+                    '<code>EMAIL_FORCE_TO</code>',
+                    '<code>' . EMAIL_FORCE_TO . '</code>'
+                )
             );
-            echo '</p>';
         }
+
+        if ($this->get_config_diffs()) {
+            echo '<div class="notice notice-error">';
+            echo '<p>';
+            esc_html_e('A plugin is overriding the environment configuration.', 'email-smtp');
+            echo '</p>';
+            echo '</div>';
+        }
+
+        echo '<p>';
+        echo __('Current configuration:', 'email-smtp');
+        echo '</p>';
+
+        echo $this->display_config();
 
         // Test form
-        echo '<hr>';
 
         echo '<form method="POST" action="' . admin_url('admin.php') . '">';
         echo '<p>' . __('Send a test message:', 'email-smtp') . '</p>';
@@ -219,10 +192,20 @@ class EmailSmtp
                 $_REQUEST['to'],
                 __('Email / SMTP test message', 'email-smtp'),
                 sprintf(
-                /* translators: Placeholder is the website URL */
-                    __('Email sending from website %s is working.', 'email-smtp'),
-                    get_home_url()
-                )
+                    '<p>%s</p>',
+                    sprintf(
+                    /* translators: Placeholder is the website URL */
+                        __('Email sending from website %s is working.', 'email-smtp'),
+                        sprintf(
+                            '<a href="%s">%s</a>',
+                            admin_url(sprintf('%s?page=%s', self::PARENT_SLUG, self::PAGE_SLUG)),
+                            get_home_url()
+                        )
+                    )
+                ),
+                [
+                    'Content-Type: text/html',
+                ]
             );
 
             $output = ob_get_clean();
@@ -234,7 +217,7 @@ class EmailSmtp
             // Authentication obfuscation
             $output = preg_replace_callback('/(334 .*?\n.*?: )([^\n:]+)/i', function ($m) {
                 return $m[1] . str_pad('', strlen($m[2]) * 3, '•');
-            }, $output);
+            },                              $output);
 
             set_site_transient('email_smtp_test_output', sprintf(
                 '<div class="notice notice-%s"><p>%s <code>%s</code></p>%s</div>',
@@ -245,43 +228,64 @@ class EmailSmtp
             ));
         }
 
-        wp_redirect(admin_url('tools.php?page=' . self::SLUG));
+        wp_redirect(admin_url('tools.php?page=' . self::PAGE_SLUG));
         exit();
     }
 
     /**
-     * @param array $config
+     * @return string
      */
-    private function display_config($config)
+    private function display_config()
     {
-        echo '<table>';
+        $mailer = $this->get_mailer_config();
+        $env = $this->get_env_config();
 
-        foreach (self::PHPMAILER_PROPERTIES as $key => $constant) {
-            if (!isset($config[$key])) {
+        // password obfuscation
+        foreach ([&$env, &$mailer] as &$config) {
+            if (empty($config[self::PROP_PASS])) {
                 continue;
             }
-
-            $value = $config[$key];
-
-            // Password obfuscation
-            if (stripos($key, 'PASS') !== false
-                || (defined('EMAIL_SMTP_PASSWORD') && EMAIL_SMTP_PASSWORD === $value)) {
-                $value = str_pad('', strlen($value) * 3, '•');
-            }
-            printf(
-                '<tr>'
-                . '<th scope="row" style="text-align:left;padding:.1em 2em .1em 0;">%s</th>'
-                . '<td style="font-family:monospace;">%s</td>'
-                . '</tr>',
-                strtolower(preg_replace(
-                               ['/^EMAIL_/', '/_/'],
-                               ['', ' '],
-                               self::PHPMAILER_PROPERTIES[$key]
-                           )),
-                $value
-            );
+            $config[self::PROP_PASS] = str_pad('', strlen($config[self::PROP_PASS]) * 3, '•');
         }
-        echo '</table>';
+
+        $rows = array_filter(
+            array_map(function ($property) use ($env, $mailer) {
+                $values = [
+                    $env[$property] ?? null,
+                    $mailer[$property] ?? null,
+                ];
+                // do not display row if no value at all
+                return array_filter($values)
+                    ? [
+                        'property' => $property,
+                        'values'   => $values,
+                    ]
+                    : false;
+            }, array_keys(array_merge($env, $mailer)))
+        );
+
+        $headers = ['property', 'env', 'phpmailer'];
+        !$env && array_splice($headers, 1, 1);
+
+        return sprintf(
+            '<table class="widefat striped">%s<tbody>%s</tbody></table>',
+            sprintf(
+                '<thead><tr>%s</tr></thead>',
+                implode('', array_map(function ($header) {
+                    return sprintf('<th scope="col">%s</th>', $header);
+                }, $headers))
+            ),
+            implode('', array_map(function ($row) use ($env) {
+                !$env && array_splice($row['values'], 0, 1);
+                return sprintf(
+                    '<tr><th scope="row">%s</th>%s</tr>',
+                    $row['property'],
+                    implode('', array_map(function ($value) {
+                        return sprintf('<td>%s</td>', $value ? sprintf('<code>%s</code>', $value) : '');
+                    }, $row['values']))
+                );
+            }, $rows))
+        );
     }
 
     private function get_phpmailer()
@@ -292,6 +296,7 @@ class EmailSmtp
         if (!($phpmailer instanceof PHPMailer)) {
             require_once ABSPATH . WPINC . '/PHPMailer/PHPMailer.php';
             require_once ABSPATH . WPINC . '/PHPMailer/SMTP.php';
+            require_once ABSPATH . WPINC . '/PHPMailer/Exception.php';
             $phpmailer = new PHPMailer(true);
         }
 
@@ -304,64 +309,42 @@ class EmailSmtp
     }
 
     /**
-     * Check that PHPMailer configuration is correctly overwritten
-     * @return boolean True if configuration is correct, false otherwise
+     * @return array Properties that are different in env to PhpMailer configs
      */
-    private function check_phpmailer_configuration()
+    private function get_config_diffs(): array
     {
-        if (!defined('EMAIL_SMTP_HOST') || EMAIL_SMTP_HOST === null) {
-            return true;
-        }
-
-        $phpmailer = $this->get_phpmailer();
-
-        if ($phpmailer->Mailer !== 'smtp') {
-            return false;
-        }
-
-        foreach (self::PHPMAILER_PROPERTIES as $property => $constant) {
-            if (defined($constant)) {
-                if ($phpmailer->{$property} !== constant($constant)) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
+        $env = $this->get_env_config();
+        $mailer = $this->get_mailer_config();
+        return array_filter(array_keys($env), function ($property) use ($env, $mailer) {
+            return array_key_exists($property, $mailer) && $mailer[$property] === $env[$property] ? false : $property;
+        });
     }
 
-    private function get_mailer_configuration()
+    /**
+     * @return array
+     */
+    private function get_mailer_config(): array
+    {
+        $phpmailer = $this->get_phpmailer();
+        $config = [];
+        foreach (array_keys(self::PHPMAILER_PROPERTIES) as $property) {
+            $config[$property] = $phpmailer->{$property} ?? null;
+        }
+        return $config;
+    }
+
+    private function get_env_config()
     {
         $config = [];
 
-        if (defined('EMAIL_FROM_EMAIL') && EMAIL_FROM_EMAIL !== null) {
-            $config['From'] = EMAIL_FROM_EMAIL;
-            $config['Sender'] = EMAIL_FROM_EMAIL;
-        }
-
-        if (defined('EMAIL_FROM_NAME') && EMAIL_FROM_NAME !== null) {
-            $config['FromName'] = EMAIL_FROM_NAME;
-        }
-
-        if (defined('EMAIL_SMTP_HOST') && EMAIL_SMTP_HOST !== null) {
+        if (defined('EMAIL_SMTP_HOST') && !empty(EMAIL_SMTP_HOST)) {
             $config['Mailer'] = 'smtp';
-
-            // set default from email and name if smtp is on
-            if (!defined('EMAIL_FROM_EMAIL') || EMAIL_FROM_EMAIL === null) {
-                $from_email = get_option('admin_email');
-                $config['From'] = $from_email;
-                $config['Sender'] = $from_email;
-            }
-
-            if (!defined('EMAIL_FROM_NAME') || EMAIL_FROM_NAME === null) {
-                $config['FromName'] = get_bloginfo('name');
-            }
         }
 
         /** @var PHPMailer $phpmailer */
         foreach (self::PHPMAILER_PROPERTIES as $property => $constant) {
-            if (defined($constant) && constant($constant) !== null) {
-                $config[$property] = constant($constant);
+            if (defined($constant) && ($value = constant($constant)) !== null) {
+                $config[$property] = $value;
             }
         }
 
@@ -374,7 +357,7 @@ class EmailSmtp
     public function phpmailer_init($phpmailer)
     {
         /** @var PHPMailer $phpmailer */
-        foreach ($this->get_mailer_configuration() as $property => $value) {
+        foreach ($this->get_env_config() as $property => $value) {
             $phpmailer->{$property} = $value;
         }
     }
@@ -415,7 +398,7 @@ class EmailSmtp
             return preg_replace_callback('/^.*(To|Cc|Bcc):(.*)$/i', function ($m) use (&$indexes) {
                 $index = $indexes[$m[1]]++;
                 return sprintf("X-DevRewrite-%s:%s", $m[1] . ($index > 1 ? $index : ''), $m[2]);
-            }, $header);
+            },                           $header);
         }, $args['headers']);
 
         return $args;
